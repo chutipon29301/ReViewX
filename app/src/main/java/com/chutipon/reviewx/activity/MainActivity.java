f832cb3fbe66daac501b674a813d1a6d4f53479f
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-07 14:40:55 +0700

    Add MainFragment

--- app/src/main/java/com/chutipon/reviewx/activity/MainActivity.java
+++ app/src/main/java/com/chutipon/reviewx/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.chutipon.reviewx.R;
+import com.chutipon.reviewx.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, MainFragment.getInstance(), "MainFragment")
+                    .commit();
+        }
+        initInstance(savedInstanceState);
+    }
+
+    private void initInstance(Bundle savedInstanceState) {
+
     }
 }
