1ae08371c80e02cc4cc58fec8570901220fbbcc4
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-07 14:32:53 +0700

    Initialize Contextor

--- app/src/main/java/com/chutipon/reviewx/MainApplication.java
+++ app/src/main/java/com/chutipon/reviewx/MainApplication.java
@@ -2,10 +2,16 @@ package com.chutipon.reviewx;
 
 import android.app.Application;
 
+import com.chutipon.reviewx.util.Contextor;
+
 /**
  * Created by admin on 7/12/2017 AD.
  */
 
 public class MainApplication extends Application {
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Contextor.getInstance().init(getApplicationContext());
+    }
 }
