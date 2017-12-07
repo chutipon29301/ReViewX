f82920d1914ea181a480c3e48f21a37d381307ca
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-07 16:02:18 +0700

    Complete Facebook login and create multiple activity

--- app/src/main/java/com/chutipon/reviewx/activity/MainActivity.java
+++ app/src/main/java/com/chutipon/reviewx/activity/MainActivity.java
@@ -1,34 +1,73 @@
 package com.chutipon.reviewx.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.chutipon.reviewx.R;
-import com.chutipon.reviewx.fragment.LoginFragment;
-import com.chutipon.reviewx.fragment.MainFragment;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            //TODO: Check login status
-
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.contentContainer, LoginFragment.getInstance(), "Login Fragment")
-                    .commit();
-
-//            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.contentContainer, MainFragment.getInstance(), "MainFragment")
-//                    .commit();
+        if (isLoggedIn()){
+            //TODO: start home activity of preference activity
+            AccessToken accessToken = AccessToken.getCurrentAccessToken();
+//            Intent intent = new Intent();
         }
+
         initInstance(savedInstanceState);
     }
 
     private void initInstance(Bundle savedInstanceState) {
+        callbackManager = CallbackManager.Factory.create();
 
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("MainActivity", "Login success");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("MainActivity", "Login cancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d("MainActivity", "Login error");
+                exception.printStackTrace();
+            }
+        });
     }
+
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
