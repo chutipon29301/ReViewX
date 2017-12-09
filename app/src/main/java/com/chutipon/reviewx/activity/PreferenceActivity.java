252f521b0559b9023ad587d0d568f122f7957d21
mingyanisa <mingignim@hotmail.com>
2017-12-09 20:41:11 +0700

    add custom view

--- app/src/main/java/com/chutipon/reviewx/activity/PreferenceActivity.java
+++ app/src/main/java/com/chutipon/reviewx/activity/PreferenceActivity.java
@@ -1,14 +1,29 @@
 package com.chutipon.reviewx.activity;
 
 import com.chutipon.reviewx.R;
+import com.chutipon.reviewx.adapter.PreferenceAdapter;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.GridView;
 
 public class PreferenceActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
+        initinstance(savedInstanceState);
+    }
+
+    private void initinstance(Bundle savedInstanceState) {
+        toolbar = findViewById(R.id.toolbar);
+        gridView = findViewById(R.id.gridView);
+
+        setSupportActionBar(toolbar);
+
+        gridView.setAdapter(new PreferenceAdapter());
     }
 }
