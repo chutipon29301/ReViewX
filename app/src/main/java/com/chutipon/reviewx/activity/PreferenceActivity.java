21c17d43017ef39dfa10b654eea1d0b21f660143
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-09 20:42:31 +0700

    Modify preference activity

--- app/src/main/java/com/chutipon/reviewx/activity/PreferenceActivity.java
+++ app/src/main/java/com/chutipon/reviewx/activity/PreferenceActivity.java
@@ -1,14 +1,28 @@
 package com.chutipon.reviewx.activity;
 
 import com.chutipon.reviewx.R;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.GridView;
 
 public class PreferenceActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    GridView gridView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
+
+        initInstance(savedInstanceState);
+    }
+
+    private void initInstance(Bundle savedInstanceState) {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
