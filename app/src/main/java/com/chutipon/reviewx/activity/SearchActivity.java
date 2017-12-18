de395ba4a244c04b30ef341b07aa5b7ec8e33f09
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-18 13:22:39 +0700

    Add search activity

--- app/src/main/java/com/chutipon/reviewx/activity/SearchActivity.java
+++ app/src/main/java/com/chutipon/reviewx/activity/SearchActivity.java
@@ -2,29 +2,30 @@ package com.chutipon.reviewx.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.chutipon.reviewx.R;
-import com.chutipon.reviewx.adapter.SearchAdapter;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
+import java.util.ArrayList;
+
 public class SearchActivity extends AppCompatActivity {
-    private static String TAG = "SearchActivity";
+    private static final String TAG = "SearchActivity";
     private static SearchActivity instance;
-    private RecyclerView searchRecycler;
-    MaterialSearchView searchView;
+    private MaterialSearchView searchView;
+
+    public static SearchActivity getInstance() {
+        return instance;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate: called");
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         instance = this;
@@ -32,15 +33,9 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void initInstance(Bundle savedInstanceState) {
-
-        searchRecycler = findViewById(R.id.searchRecycler);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         searchView = findViewById(R.id.search_view);
-
-        SearchAdapter.getInstance().init(getBaseContext());
-
-        searchRecycler.setAdapter(SearchAdapter.getInstance());
-        searchRecycler.setLayoutManager(new LinearLayoutManager(getBaseContext()));
-
         searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -67,16 +62,12 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        searchView.setVoiceSearch(false);
     }
 
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-
-        super.onPostCreate(savedInstanceState);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_search, menu);
 
         MenuItem item = menu.findItem(R.id.action_search);
         searchView.setMenuItem(item);
@@ -84,20 +75,19 @@ public class SearchActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    searchView.setQuery(searchWrd, false);
+                }
+            }
 
-    public static SearchActivity getInstance() {
-        return instance;
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
-    public void redirect(Class cls) {
-        Intent intent = new Intent(SearchActivity.this, cls);
-        startActivity(intent);
-    }
-    public void redirectFragment(Fragment frag){
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.contentContainer,frag)
-                .commit();
-    }
-
-
-
 }
