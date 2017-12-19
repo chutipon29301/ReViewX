555a05ff65b4c07e55a819c35bb1cfe502fdb6be
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-19 21:14:13 +0700

    CleanUp mapfragment

--- app/src/main/java/com/chutipon/reviewx/fragment/MapFragment.java
+++ app/src/main/java/com/chutipon/reviewx/fragment/MapFragment.java
@@ -27,12 +27,6 @@ public class MapFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_map,container,false);
-        initInstance(rootView,savedInstanceState);
-        return rootView;
-    }
-
-    private void initInstance(View rootView, Bundle savedInstanceState) {
-
+        return inflater.inflate(R.layout.fragment_map,container,false);
     }
 }
