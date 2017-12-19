ffee7ade2c25e15bb6c75f726690b81948625038
mingyanisa <mingignim@hotmail.com>
2017-12-19 07:45:02 +0700

    add scorebar

--- app/src/main/java/com/chutipon/reviewx/adapter/ReviewListAdapter.java
+++ app/src/main/java/com/chutipon/reviewx/adapter/ReviewListAdapter.java
@@ -6,10 +6,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.chutipon.reviewx.R;
 import com.chutipon.reviewx.activity.HomeActivity;
 import com.chutipon.reviewx.activity.ReadReviewActivity;
+import com.chutipon.reviewx.activity.ReviewListActivity;
+import com.chutipon.reviewx.manager.MovieReviewManager;
+import com.chutipon.reviewx.manager.MovieSuggestionManager;
+import com.makeramen.roundedimageview.RoundedTransformationBuilder;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+import com.squareup.picasso.Transformation;
 
 
 /**
@@ -19,6 +27,7 @@ import com.chutipon.reviewx.activity.ReadReviewActivity;
     public class ReviewListAdapter extends RecyclerView.Adapter< ReviewListAdapter.ViewHolder> implements View.OnClickListener {
 
    private LayoutInflater mInflater;
+
     private ReviewListAdapter(){
 
     }
@@ -41,18 +50,19 @@ import com.chutipon.reviewx.activity.ReadReviewActivity;
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = mInflater.inflate(R.layout.view_reviewlist_custom,parent,false);
         parent.setOnClickListener(this);
+
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ReviewListAdapter.ViewHolder holder, int position) {
-
+        holder.bind(position);
     }
 
 
     @Override
     public int getItemCount() {
-        return 6;
+        return MovieReviewManager.getInstance().getSize();
     }
 
     @Override
@@ -61,15 +71,50 @@ import com.chutipon.reviewx.activity.ReadReviewActivity;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private int position;
+        private Target reviewerImg;
+        private TextView movieName;
+        private TextView firstword;
+        private TextView secondword;
+        private TextView thirdword;
+        private TextView score;
+
         public ViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
+            initInstance(itemView);
+        }
+
+        private void initInstance(View itemView) {
+
+
         }
 
 
         @Override
         public void onClick(View view) {
-            HomeActivity.getInstance().redirect(ReadReviewActivity.class);
+//            HomeActivity.getInstance().redirect(ReviewListActivity.class, "movieID", MovieSuggestionManager.getInstance().getMovieSuggestionInfoAtIndex(position).getId());
+
         }
+
+        public void bind(int position) {
+//            this.position = position;
+//            Transformation transformation = new RoundedTransformationBuilder()
+//                    .cornerRadiusDp(30)
+//                    .oval(true)
+//                    .build();
+//            Picasso.with(itemView.getContext())
+//                    .load(MovieReviewManager.getInstance().getMovieReviewInfoDaoAtIndex(position))
+//                    .resize(150, 150)
+//                    .centerCrop()
+//                    .transform(transformation)
+//                    .into(reviewerImg);
+//            movieName.setText(MovieSuggestionManager.getInstance().getMovieSuggestionInfoAtIndex(position).getTitle());
+//            firstword.setText(MovieSuggestionManager.getInstance().getMovieSuggestionInfoAtIndex(position).getReleaseDate());
+//            score.setText(String.valueOf(MovieSuggestionManager.getInstance().getMovieSuggestionInfoAtIndex(position).getVoteAverage()));
+
+        }
+
+
     }
 }
