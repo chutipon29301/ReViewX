9fc617f30f3ea438d110cdc05f38eb85b1a37250
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-19 23:07:36 +0700

    clean up random fragment

--- app/src/main/java/com/chutipon/reviewx/fragment/RandomFragment.java
+++ app/src/main/java/com/chutipon/reviewx/fragment/RandomFragment.java
@@ -1,7 +1,6 @@
 package com.chutipon.reviewx.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.chutipon.reviewx.R;
