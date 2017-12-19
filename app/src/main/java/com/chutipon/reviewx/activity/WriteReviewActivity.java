ab9ba439a2d90290d1c18f39879f8266839691b5
Nicha <NichaRoj@gmail.com>
2017-12-19 23:49:54 +0700

    Beautified WriteReview
    also for find stuff in RealmManager, it should have openLocalInstance
    stuff instead of runTransaction

--- app/src/main/java/com/chutipon/reviewx/activity/WriteReviewActivity.java
+++ app/src/main/java/com/chutipon/reviewx/activity/WriteReviewActivity.java
@@ -3,6 +3,7 @@ package com.chutipon.reviewx.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Scroller;
 
+import com.chutipon.reviewx.MinMaxFilter;
 import com.chutipon.reviewx.R;
 import com.chutipon.reviewx.dao.AddReviewDao;
 import com.chutipon.reviewx.manager.AddReviewManager;
@@ -64,6 +66,7 @@ public class WriteReviewActivity extends AppCompatActivity implements View.OnCli
         secondWord.setHorizontallyScrolling(true);
         thirdWord.setHorizontallyScrolling(true);
         score.setMaxLines(1);
+        score.setFilters(new InputFilter[]{new MinMaxFilter(this,"0","100")});
         movieImage = findViewById(R.id.movieImage);
 
         questionEntry = findViewById(R.id.reviewText);
