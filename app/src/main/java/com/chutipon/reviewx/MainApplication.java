f696ddb1c83c8c0c4bb9665650c9c49cde24ed0c
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-20 12:18:09 +0700

    Add fabris

--- app/src/main/java/com/chutipon/reviewx/MainApplication.java
+++ app/src/main/java/com/chutipon/reviewx/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.chutipon.reviewx.util.Contextor;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -15,6 +17,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Contextor.getInstance().init(getApplicationContext());
         Realm.init(this);
         RealmConfiguration config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
